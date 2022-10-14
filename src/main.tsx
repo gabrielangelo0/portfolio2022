02d3e02bfa462d55082e417f435f655e77cd1fa6
Gabriel Angelo Lira Dias <gabrielangelolira@gmail.com>
2022-10-14 20:30:33 -0300

    seventhy commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import App from './App'
 import { GlobalStyles } from './styles/global'
 import { DefaultTheme } from './styles/themes/default'
-import BackgroundImg from './assets/background-image.svg'
 import { MainContainer } from './pages/Home/styles'
+import { ThemeProvider } from 'styled-components'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
